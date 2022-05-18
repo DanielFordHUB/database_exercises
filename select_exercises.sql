7ef7f773ca2873e8ac58670a3bd45ea6599af9bc
Daniel Ford <daniel.ford791@gmail.com>
2022-05-18 16:36:11 -0500

    added a bunch of exercises 5/18/2022

---
+++ select_exercises.sql
@@ -0,0 +1,44 @@
+use albums_db;
+show tables;
+describe albums;
+select * from albums;
+select count(distinct artist)
+from albums;
+show keys from albums where key_name = 'primary';
+
+
+-- how many rows?
+-- 31
+-- how many unique artist names are in each table?
+-- 23
+-- what is primary key?
+-- PRIMARY
+-- what is the oldest release date? what is the most current?
+-- 2011. 1971.
+
+select *
+from albums
+where artist = "pink floyd";
+select * from albums where artist = "the beatles";
+select genre
+from albums
+where name = "nevermind";
+select *
+from albums
+where release_date between '1990' and '1999';
+select * from albums where sales < 20;
+select * from albums where genre = "rock";
+
+/* a. The name of all albums by Pink Floyd
+The dark side of the moon, The Wall
+b. The year Sgt. Pepper's Lonely Hearts Club Band was released
+1967
+c. The genre for the album Nevermind
+grunge, alt rock
+d. Which albums were released in the 1990s
+id 5,12,13,14,19,21,22,26,27,28,30
+e. Which albums had less than 20 million certified sales
+id 9, 11, 15, 17, 19, 21, 22, 23, 24, 25, 26, 28, 29
+f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"? 
+id 15, 20, 23, 24, 30
+*/
---
+++ tables_exercise.sql
@@ -0,0 +1,13 @@
+use employees;
+
+show tables;
+
+describe employees;
+-- Which table(s) do you think contain a numeric type column? all but dept
+-- Which table(s) do you think contain a string type column? all but salaries
+-- Which table(s) do you think contain a date type column? all
+-- What is the relationship between the employees and the departments tables?
+
+describe dept_emp;
+show create table dept_manager;
+
