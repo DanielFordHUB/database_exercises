501db0d6acc0f27849151b0205990c7866103b2c
Daniel Ford <daniel.ford791@gmail.com>
2022-05-18 16:59:06 -0500

    updatedd answers, fleshed out

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,19 +2,23 @@ use albums_db;
 show tables;
 describe albums;
 select * from albums;
+select count(*) from albums;
+-- how many rows^^^^^^^
 select count(distinct artist)
 from albums;
+-- unique artists^^^^^^^^^
 show keys from albums where key_name = 'primary';
-
+select min(release_date) from albums;
+select max(release_date) from albums;
 
 -- how many rows?
 -- 31
 -- how many unique artist names are in each table?
 -- 23
 -- what is primary key?
--- PRIMARY
+-- id
 -- what is the oldest release date? what is the most current?
--- 2011. 1971.
+-- 2011. 1967.
 
 select *
 from albums
@@ -28,7 +32,9 @@ from albums
 where release_date between '1990' and '1999';
 select * from albums where sales < 20;
 select * from albums where genre = "rock";
-
+-- more exact search^^^^^^^^^^^
+select * from albums where genre like "%rock%";
+-- more broad because of %LIKE%
 /* a. The name of all albums by Pink Floyd
 The dark side of the moon, The Wall
 b. The year Sgt. Pepper's Lonely Hearts Club Band was released
@@ -40,5 +46,5 @@ id 5,12,13,14,19,21,22,26,27,28,30
 e. Which albums had less than 20 million certified sales
 id 9, 11, 15, 17, 19, 21, 22, 23, 24, 25, 26, 28, 29
 f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"? 
-id 15, 20, 23, 24, 30
+id 15, 20, 23, 24, 30 // 
 */
